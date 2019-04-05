a103f0e7efb667f63c96fa67135f50c88d86d7f7
Big Mogician <sunaoran@youzan.com>
2019-04-06 01:09:05 +0800

    remove some console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,6 @@ export class AppComponent {
 
     if (electronService.isElectron()) {
       console.log("Mode electron");
-      console.log("Electron ipcRenderer", electronService.ipcRenderer);
-      console.log("NodeJS childProcess", electronService.childProcess);
     } else {
       console.log("Mode web");
     }
